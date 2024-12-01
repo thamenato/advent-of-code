36f21b7c234d4de2139d42d0792b6d30c47ce489
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-11-30 20:25:19 -0500

    chore: prepare repo for 2024 challenges

--- flake.nix
+++ flake.nix
@@ -6,23 +6,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          delve
-          go
-          go-tools
-          golangci-lint
-          gopls
-          pre-commit
-        ];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            delve
+            go
+            go-tools
+            golangci-lint
+            gopls
+            pre-commit
+            uv
+            python3
+          ];
+        };
+      }
+    );
 }

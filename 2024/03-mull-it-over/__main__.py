847269d01be263acbd241d77deb77b4d6ed29082
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-12-03 11:09:00 -0500

    feat: add solution for part 2 of day 3

--- 2024/03-mull-it-over/__main__.py
+++ 2024/03-mull-it-over/__main__.py
@@ -8,9 +8,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("part", type=int)
 parser.add_argument("filename")
 
+_re_mul = re.compile(r"mul\((?P<left>\d{0,3}),(?P<right>\d{0,3})\)")
+_re_do = re.compile(r"don\'t\(\)|do\(\)")
+
 
 def part_one(data):
-    groups = re.findall(r"mul\((?P<left>\d{0,3}),(?P<right>\d{0,3})\)", data)
+    groups = _re_mul.findall(data)
     total = 0
     for g in groups:
         total += int(g[0]) * int(g[1])
@@ -19,7 +22,35 @@ def part_one(data):
 
 
 def part_two(data):
-    pass
+    modes = _re_do.finditer(data)
+    just_do = True
+
+    strings = []
+    pivot = 0
+    for m in modes:
+        if m.group() == "don't()":
+            if just_do:
+                strings.append(data[pivot : m.start()])
+                pivot = m.end()
+                just_do = False
+            else:
+                pivot = m.end()
+        if m.group() == "do()":
+            if just_do:
+                strings.append(data[pivot : m.start()])
+                pivot = m.end()
+            else:
+                pivot = m.end()
+                just_do = True
+
+    strings.append(data[pivot:])
+
+    total = 0
+    for s in strings:
+        for g in _re_mul.findall(s):
+            total += int(g[0]) * int(g[1])
+
+    return total
 
 
 def main():

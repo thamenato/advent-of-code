216a490e9fde29c7a80c0764bdc49515b1b02e59
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-12-04 23:04:15 -0500

    feat: push part 2 for day 04

--- 2024/04-ceres-search/__main__.py
+++ 2024/04-ceres-search/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+from dataclasses import dataclass
 from importlib.resources import files
 
 from IPython import embed
@@ -18,41 +19,77 @@ def _check_line(line: str) -> int:
     return found
 
 
+def _get_diagonals(text: str):
+    break_line = text.index("\n")
+    num_of_lines = len(text.split("\n"))
+
+    diagonals = []
+    for row in range(0, num_of_lines - 4):
+        line = ""
+        char_idx = row * break_line + row
+        for i in range(0, break_line):
+            line += text[char_idx]
+            char_idx += break_line + 2
+            if char_idx >= len(text):
+                break
+
+        diagonals.append(line)
+
+    for col in range(1, break_line - 3):
+        line = ""
+        char_idx = col
+        for i in range(0, break_line - col):
+            line += text[char_idx]
+            char_idx += break_line + 2
+        diagonals.append(line)
+
+    return diagonals
+
+
 def part_one(data):
     total = 0
 
     break_line = data.index("\n")
-    num_of_lines = len(data.split("\n"))
 
     # check horizontal
     for line in data.split("\n"):
         total += _check_line(line)
 
     # check vertical
-    for i in range(0, break_line):
-        line = "".join([data[c] for c in range(i, len(data), break_line + 1)])
+    for row in range(0, break_line):
+        line = "".join([data[c] for c in range(row, len(data), break_line + 1)])
         total += _check_line(line)
 
-    # check diagonal
-    for i in range(0, len(data) - num_of_lines * len("XMAS"), num_of_lines + 1):
-        for j in range(0, break_line - len("XMAS") + 1):
-            line = ""
-            for c in range(i + j, len(data), break_line + 2):
-                if data[c] == "\n":
-                    break
-                line += data[c]
+    # check diagonals
+    for line in _get_diagonals(data):
+        total += _check_line(line)
 
-            print(line)
-            print(f"new_column {j}")
-
-        print(f"new_line {i}")
-        # line = "".join([data[c] for c in range(i + j, len(data), break_line + 2)])
+    mirror = "\n".join([l[::-1] for l in data.split()])
+    for line in _get_diagonals(mirror):
+        total += _check_line(line)
 
     return total
 
 
 def part_two(data):
-    pass
+    _re_mas = re.compile(r"MAS|SAM")
+    matrix = data.split()
+
+    found = 0
+    for i, line in enumerate(matrix):
+        if i < 1 or i > len(matrix) - 2:
+            continue
+        for a in re.finditer("A", line):
+            try:
+                left = f"{matrix[i - 1][a.start() - 1]}{matrix[i][a.start()]}{matrix[i + 1][a.start() + 1]}"
+                right = f"{matrix[i + 1][a.start() - 1]}{matrix[i][a.start()]}{matrix[i - 1][a.start() + 1]}"
+
+                if _re_mas.match(left) and _re_mas.match(right):
+                    found += 1
+            except IndexError:
+                continue
+
+    return found
 
 
 def main():

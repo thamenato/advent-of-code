a70bc4f3f5259df121a0217de82ce196c6c042f9
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-12-08 11:53:31 -0500

    feat: push solutions for day 08

--- 2024/08-resonant-collinearity/__main__.py
+++ 2024/08-resonant-collinearity/__main__.py
@@ -1,12 +1,25 @@
 import argparse
+import itertools
+import math
 import re
+from dataclasses import astuple, dataclass
 from importlib.resources import files
-
-from IPython import embed
 
 parser = argparse.ArgumentParser()
 parser.add_argument("part", type=int)
 parser.add_argument("filename")
+
+
+@dataclass
+class Point:
+    row: int
+    col: int
+
+    def __add__(self, other):
+        return Point(row=self.row + other.row, col=self.col + other.col)
+
+    def __sub__(self, other):
+        return Point(row=self.row - other.row, col=self.col - other.col)
 
 
 class Map:
@@ -14,7 +27,8 @@ class Map:
 
     def __init__(self, data):
         self._chart = data.split("\n")
-        self._edge = len(self._chart)
+        self._max_row = len(self._chart)
+        self._max_col = len(self._chart[0])
         self.antennas = self._find_antennas()
 
     @property
@@ -29,21 +43,71 @@ class Map:
         antennas = {}
         for i, line in enumerate(self.chart):
             for m in self._re_antennas.finditer(line):
+                point = Point(row=i, col=m.start())
                 if m.group() in antennas:
-                    antennas[m.group()].append((i, m.start()))
+                    antennas[m.group()].append(point)
                 else:
-                    antennas[m.group()] = [(i, m.start())]
+                    antennas[m.group()] = [point]
 
         return antennas
+
+    def out_of_bounds(self, point):
+        if point.row < 0 or point.col < 0:
+            return True
+
+        if point.row >= self._max_row or point.col >= self._max_col:
+            return True
+
+        return False
 
 
 def part_one(data):
     my_map = Map(data)
-    print(my_map.antennas)
+
+    locations = set()
+    for points in my_map.antennas.values():
+        line_list = list(itertools.combinations(points, 2))
+        for line in line_list:
+            slope = line[1] - line[0]
+
+            attempt = line[0] - slope
+
+            if not my_map.out_of_bounds(attempt):
+                locations.add(astuple(attempt))
+
+            attempt = line[1] + slope
+
+            if not my_map.out_of_bounds(attempt):
+                locations.add(astuple(attempt))
+
+    return len(locations)
 
 
 def part_two(data):
-    pass
+    my_map = Map(data)
+
+    locations = set()
+
+    for points in my_map.antennas.values():
+        line_list = list(itertools.combinations(points, 2))
+        for line in line_list:
+            locations.add(astuple(line[0]))
+            locations.add(astuple(line[1]))
+            slope = line[1] - line[0]
+
+            attempt = line[0] - slope
+            while not my_map.out_of_bounds(attempt):
+                print(attempt)
+                locations.add(astuple(attempt))
+                attempt = attempt - slope
+
+            attempt = line[1] + slope
+            while not my_map.out_of_bounds(attempt):
+                print(attempt)
+                locations.add(astuple(attempt))
+                attempt = attempt + slope
+
+    return len(locations)
 
 
 def main():

13d27ce254a194df724eb0e6c358bf02fe983647
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-12-11 11:54:58 -0500

    feat: push part1 for day 11

--- 2024/11-plutonian-pebbles/__main__.py
+++ 2024/11-plutonian-pebbles/__main__.py
@@ -1,19 +1,66 @@
 import argparse
+from dataclasses import dataclass
 from importlib.resources import files
-
-from IPython import embed
 
 parser = argparse.ArgumentParser()
 parser.add_argument("part", type=int)
 parser.add_argument("filename")
 
 
+def _process_stone(value):
+    if int(value) == 0:
+        return ["1"]
+
+    if len(value) % 2 == 0:
+        size = int(len(value))
+        half = int(size / 2)
+        left = value[:half]
+        right = str(int(value[half:]))  # remove trailing zeroes
+        return [left, right]
+
+    value = str(int(value) * 2024)
+    return [value]
+
+
+# def _process_blink(stone_list):
+#     after_blink = {}
+#     for value in stone_list:
+#         if value in after_blink:
+#             after_blink[value] += 1
+
+#         stones = _process_stone(value)
+#         after_blink += processed
+
+#     result = after_blink.copy()
+
+
 def part_one(data):
-    pass
+    blinks = 25
+
+    result = data.split()
+    for i in range(0, blinks):
+        after_blink = []
+        for stone in result:
+            processed = _process_stone(stone)
+            after_blink += processed
+
+        result = after_blink.copy()
+
+    return len(result)
 
 
 def part_two(data):
-    pass
+    blinks = 25
+
+    result = data.split()
+    for i in range(0, blinks):
+        after_blink = []
+        for stone in result:
+            processed = _process_stone(stone)
+            after_blink += processed
+
+        result = after_blink.copy()
+    return len(result)
 
 
 def main():
